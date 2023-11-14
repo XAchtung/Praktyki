a2d8c5a2cec0eb6c8629db14a94c3d52fc906d89
XAchtung <krisbery@o2.pl>
2023-11-14 18:36:23 +0100

    update dodatkowe

--- 14.11/dodatkowe/myscript.js
+++ 14.11/dodatkowe/myscript.js
@@ -1,5 +1,5 @@
 const genPicture = () => {
-	fetch('https://jsonplaceholder.typicode.com/posts/3')
+	fetch('https://jsonplaceholder.typicode.com/posts')
 	.then((response) => {
 		if(!response.ok) {
 			throw new Error("Network response was not OK")
@@ -7,11 +7,12 @@ const genPicture = () => {
 		return response.json()
 	})
 	.then((myJson) => {
+        const json = myJson.filter((json) => json.id == 3)[0];
 		let h1 = document.createElement('h1');
-		h1.src = myJson["title"];
+		h1.src = json["title"];
         document.querySelector('body').appendChild(h1)
         let div = document.createElement("div");
-        div.textContent = myJson["body"];
+        div.textContent = json["body"];
         document.querySelector("h1").appendChild(div);
 	})
 	.catch((error) => console.error('There has been some kind of problem: ', error))
